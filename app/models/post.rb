8b0453d8b00d2b46edcae22741c1457de8782053
MayPhoo-Github <mayphoowai14@gmail.com>
2022-01-07 14:12:38 +0630

    [main]fix bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user, :foreign_key => :created_user_id, :primary_key => :id, optional: true, :dependent => :destroy
+  belongs_to :user, :foreign_key => :created_user_id, :primary_key => :id, optional: true
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
   validates_inclusion_of :public_flag, :in => [true, false]
